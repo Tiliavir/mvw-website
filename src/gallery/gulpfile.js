c0ff45539851a0cac32fe9260874693ff3d33b1a
Markus Lindenmann <markus.lindenmann@outlook.com>
2016-06-13 09:47:48 +0000

    Updated gulpfile.js

--- src/gallery/gulpfile.js
+++ src/gallery/gulpfile.js
@@ -1,5 +1,6 @@
 (function (require) {
   "use strict";
+
   const gulp = require("gulp"),
         fs = require("fs"),
         lazypipe = require("lazypipe"),
@@ -12,63 +13,63 @@
   const img = require("./ImageProcessor.js");
 
   const paths = {
-    dest: "./build/",
-    assets: "./assets/",
+    output: "./build/",
+    input: "./input/"
   };
 
-  function processImage(w, h, p) {
-    return lazypipe()
-      .pipe($.rename, function (path) {
-        if (/\\m$/.test(path.dirname) || /\\s$/.test(path.dirname)) {
-          path.dirname = path.dirname.substring(0, path.dirname.length - 2);
-        }
-        path.dirname += (p ? ("/" + p) : "");
-      })
-      .pipe(parallel, $.imageResize({
-          width: w,
-          height: h,
-          crop: false,
-          upscale: false,
-          format: "jpg",
-          quality: 0.7
-        }),
-        os.cpus().length)
-      .pipe(gulp.dest, paths.dest + "gallery/")
-      .pipe(through.obj, function (file, enc, cb) {
-        img.addSize(file, sizeOf(file.path));
-        cb(null, file);
-      });
-  }
+  gulp.task("resize", function () {
+    var processImage = function (w, h, p) {
+      return lazypipe()
+        .pipe($.rename, function (path) {
+          if (/\\m$/.test(path.dirname) || /\\s$/.test(path.dirname)) {
+            path.dirname = path.dirname.substring(0, path.dirname.length - 2);
+          }
+          path.dirname += (p ? ("/" + p) : "");
+        })
+        .pipe(parallel, $.imageResize({
+            width: w,
+            height: h,
+            crop: false,
+            upscale: false,
+            format: "jpg",
+            quality: 0.7
+          }),
+          os.cpus().length)
+        .pipe(gulp.dest, paths.output)
+        .pipe(through.obj, function (file, enc, cb) {
+          img.addSize(file, sizeOf(file.path));
+          cb(null, file);
+        });
+    };
 
-  gulp.task("gallery:resize", function () {
     var images = null;
     var state = null;
     try {
-      images = require(paths.assets + "/gallery/galleries.json");
-      state = require(paths.assets + "/gallery/state.json");
+      images = require(paths.input + "galleries.json");
+      state = require(paths.input + "state.json");
     } catch (e) {
     }
     if (!img.init(images, state)) {
       $.util.log("Could not restore gallery information - will start from scratch!");
     }
 
-    return gulp.src(paths.assets + "gallery/**/*.{png,jpg,jpeg,PNG,JPG,JPEG}",
-                    { base: paths.assets + "gallery/" })
-                .pipe(through.obj(function (chunk, enc, cb) {
+    return gulp.src(paths.input + "**/*.{png,jpg,jpeg,PNG,JPG,JPEG}",
+                    { base: paths.input })
+                /*.pipe(through.obj(function (chunk, enc, cb) {
                   img.addInformation(chunk);
                   cb(null, chunk);
-                }))
+                }))*/
                 .pipe($.rename(function (path) {
                   path.basename = img.filename.replace(/(\..{3,4})$/, "");
                   var info = path.dirname.split("\\");
                   path.dirname = path.dirname.replace(info[info.length - 1], img.foldername);
                 }))
-                .pipe(processImage(1200, 1200, null)())
-                .pipe(processImage(800, 800, "m")())
+                /*.pipe(processImage(1200, 1200, null)())
+                .pipe(processImage(800, 800, "m")())*/
                 .pipe(processImage(200, 200, "s")());
   });
 
-  gulp.task("gallery:writeInfo", ["gallery:resize"], function () {
-    img.writeFiles(fs.writeFileSync, paths.dest + "/gallery/");
+  gulp.task("default", ["resize"], function () {
+    img.writeFiles(fs.writeFileSync, paths.output);
   });
-});
+})(require);
