52d83c75198d3866347a1d03c967536ffb0ecb8d
Markus Lindenmann <Markus.Lindenmann@gmail.com>
2016-12-20 07:25:45 +0100

    gulp => ts updated news (konzert 2014) fixed suche (potentialAction) npm update

--- gulpfile.js
+++ gulpfile.js
@@ -1,133 +1,104 @@
-﻿(function (require) {
-  "use strict";
-
-  const gulp = require("gulp"),
-        fs = require("fs"),
-        path = require("path"),
-        marked = require("marked"),
-        moment = require("moment"),
-        args = require("yargs").argv,
-        $ = require("gulp-load-plugins")(),
-        navigation = require("mvw-navigation"),
-        searchIndex = require("mvw-search-index");
-
-  let isRelease = args.release || false;
-  let baseUrl = isRelease ? "http://www.mv-wollbach.de/" : "http://localhost/";
-
-  const paths = {
+"use strict";
+var fs = require("fs");
+var gulp = require("gulp");
+var moment = require("moment");
+var path = require("path");
+var yargs = require("yargs");
+var gulpLoadPlugins = require("gulp-load-plugins");
+var mvw_navigation_1 = require("mvw-navigation");
+var mvw_search_index_1 = require("mvw-search-index");
+var isRelease = yargs["default"]("release", false).boolean("release").argv;
+var baseUrl = isRelease ? "http://www.mv-wollbach.de/" : "http://localhost/";
+var navigation;
+var searchIndex = new mvw_search_index_1.SearchIndex();
+var $ = gulpLoadPlugins();
+var paths = {
     dest: "./build/",
     assets: "./assets/"
-  };
-
-  gulp.task("sitemap", function () {
-      return gulp.src([paths.dest + "**/*.html", "!**/401.html", "!**/404.html"])
-      .pipe($.sitemap({
+};
+gulp.task("sitemap", function () {
+    return gulp.src([paths.dest + "**/*.html", "!**/401.html", "!**/404.html"])
+        .pipe($.sitemap({
         siteUrl: baseUrl,
         changefreq: "monthly"
-      }))
-      .pipe(gulp.dest(paths.dest));
-  });
-
-  gulp.task("html:writeNavigation", function () {
-    navigation.init(require(paths.assets + "pages/site-structure.json"));
+    }))
+        .pipe(gulp.dest(paths.dest));
+});
+gulp.task("html:writeNavigation", function () {
+    navigation = new mvw_navigation_1.Navigation(require(paths.assets + "pages/site-structure.json"));
     fs.writeFileSync(paths.assets + "pages/siteOverviewList.pug", navigation.writeNavigation("allplain"));
     fs.writeFileSync(paths.assets + "pages/topnavigation.pug", navigation.writeNavigation("top"));
     fs.writeFileSync(paths.assets + "pages/footernavigation.pug", navigation.writeNavigation("footer"));
-  });
-
-  gulp.task("html:generatePages", ["html:writeNavigation"], function () {
-    const scope = {
-      register: require(paths.assets + "pages/data/register.json"),
-      berichte: require(paths.assets + "pages/data/berichte.json"),
-      vorstand: require(paths.assets + "pages/data/vorstand.json"),
-      termine: require(paths.assets + "pages/data/termine.json"),
-
-      jugendRegister: require(paths.assets + "pages/data/jugend-register.json"),
-      news: require(paths.assets + "pages/data/news.json"),
-      galleries: require(paths.assets + "gallery/galleries.json"),
-
-      siteTitle: "Musikverein Wollbach 1866 e.V.",
-      baseUrl: baseUrl,
+});
+gulp.task("html:generatePages", ["html:writeNavigation"], function () {
+    var scope = {
+        register: require(paths.assets + "pages/data/register.json"),
+        berichte: require(paths.assets + "pages/data/berichte.json"),
+        vorstand: require(paths.assets + "pages/data/vorstand.json"),
+        termine: require(paths.assets + "pages/data/termine.json"),
+        jugendRegister: require(paths.assets + "pages/data/jugend-register.json"),
+        news: require(paths.assets + "pages/data/news.json"),
+        galleries: require(paths.assets + "gallery/galleries.json"),
+        siteTitle: "Musikverein Wollbach 1866 e.V.",
+        baseUrl: baseUrl,
+        numberOfMusicians: 0
     };
-
     var getNumberOfMusicians = function (register) {
-      var distinctNames = {};
-      register.map(function(p) {
-          return p.name + " " + p.familyName;
-        }).forEach(function(p) {
-          distinctNames[p] = true;
-      });
-      var numberOfMusicians = Object.keys(distinctNames).length;
-      if (!numberOfMusicians || numberOfMusicians < 30 || numberOfMusicians > 70) {
-        $.util.log("Unexpected number of musicians calculated:", numberOfMusicians);
-        throw ("Unexpected number of musicians calculated: " + numberOfMusicians);
-      }
-
-      return numberOfMusicians;
+        var distinctNames = {};
+        register.map(function (p) {
+            return p.name + " " + p.familyName;
+        }).forEach(function (p) {
+            distinctNames[p] = true;
+        });
+        var numberOfMusicians = Object.keys(distinctNames).length;
+        if (!numberOfMusicians || numberOfMusicians < 30 || numberOfMusicians > 70) {
+            $.util.log("Unexpected number of musicians calculated:", numberOfMusicians);
+            throw ("Unexpected number of musicians calculated: " + numberOfMusicians);
+        }
+        return numberOfMusicians;
     };
-
     scope.numberOfMusicians = getNumberOfMusicians(scope.register);
-
     var getScope = function (file) {
-      var filename = path.basename(file.path, path.extname(file.path));
-      return {
-        marked: marked,
-        moment: moment,
-        require: require,
-
-        isAmp: false,
-        isRelease: isRelease,
-        scope: scope,
-
-        referencedFile: filename,
-        breadcrumb: filename === "index" ? null : navigation.getBreadcrumbHtml(filename)
-      };
+        var filename = path.basename(file.path, path.extname(file.path));
+        return {
+            marked: marked,
+            moment: moment,
+            require: require,
+            isAmp: false,
+            isRelease: isRelease,
+            scope: scope,
+            referencedFile: filename,
+            breadcrumb: filename === "index" ? null : navigation.getBreadcrumb(filename, true)
+        };
     };
-
-    var hasAmp = function (file) {
-      return file.data.hasAmp;
-    };
-
     return gulp.src(paths.assets + "pages/partials/**/*.pug")
-               .pipe($.replace(/^(\s*#+) /gm, "$1# "))
-               .pipe($.rename(function(path) { path.extname = ".html"; }))
-               .pipe($.grayMatter())
-               .pipe($.data(getScope))
-               .pipe($.data(searchIndex.add))
-               .pipe($.pug())
-               .pipe($.flatten())
-               .pipe(gulp.dest(paths.dest))
-/*
-               // https://github.com/pugjs/pug/issues/2367
-               .pipe($.if(hasAmp, $.data(function(file) {
-                 file.data.isAmp = true;
-                 return file.data
-               })))
-               .pipe($.if(hasAmp, $.pug()))
-               .pipe($.if(hasAmp, gulp.dest(paths.dest + "amp/")))
-*/
-    ;
-  });
-
-  gulp.task("html:minify", function () {
+        .pipe($.replace(/^(\s*#+) /gm, "$1# "))
+        .pipe($.rename(function (path) { path.ext = ".html"; }))
+        .pipe($.grayMatter())
+        .pipe($.data(getScope))
+        .pipe($.data(function (file) {
+        searchIndex.add(file, file.data);
+    }))
+        .pipe($.pug())
+        .pipe($.flatten())
+        .pipe(gulp.dest(paths.dest));
+});
+gulp.task("html:minify", function () {
     return gulp.src(paths.dest + "**/*.html")
-               .pipe($.htmlmin({
-                 sortAttributes: true,
-                 removeComments: true,
-                 collapseWhitespace: true,
-                 collapseInlineTagWhitespace: true,
-                 removeAttributeQuotes: true,
-                 conservativeCollapse: true,
-                 minifyJS: true,
-                 minifyCSS: true
-               }))
-               .pipe(gulp.dest(paths.dest));
-  });
-
-  gulp.task("search:index", function () {
+        .pipe($.htmlmin({
+        sortAttributes: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        removeAttributeQuotes: true,
+        conservativeCollapse: true,
+        minifyJS: true,
+        minifyCSS: true
+    }))
+        .pipe(gulp.dest(paths.dest));
+});
+gulp.task("search:index", function () {
     fs.writeFileSync(paths.dest + "index.json", JSON.stringify(searchIndex.getResult()));
-  });
-
-  gulp.task("default", $.sequence("html:generatePages", "search:index"));
-  gulp.task("release", $.sequence("html:generatePages", "sitemap", "html:minify", "search:index"));
-})(require);
+});
+gulp.task("default", $.sequence("html:generatePages", "search:index"));
+gulp.task("release", $.sequence("html:generatePages", "sitemap", "html:minify", "search:index"));
---
+++ gulpfile.ts
@@ -0,0 +1,136 @@
+﻿import * as fs from "fs";
+import * as File from "vinyl";
+import * as gulp from "gulp";
+import * as moment from "moment";
+import * as path from "path";
+import * as yargs from "yargs";
+import * as gulpLoadPlugins from "gulp-load-plugins";
+import { Navigation } from "mvw-navigation";
+import { SearchIndex } from "mvw-search-index";
+
+let isRelease: boolean = yargs.default("release", false).boolean("release").argv;
+let baseUrl = isRelease ? "http://www.mv-wollbach.de/" : "http://localhost/";
+let navigation: Navigation;
+let searchIndex: SearchIndex = new SearchIndex();
+let $: any = gulpLoadPlugins();
+
+const paths: {dest: string, assets: string} = {
+  dest: "./build/",
+  assets: "./assets/"
+};
+
+declare interface IPerson {
+  name: string;
+  familyName: string;
+}
+
+gulp.task("sitemap", () => {
+    return gulp.src([paths.dest + "**/*.html", "!**/401.html", "!**/404.html"])
+    .pipe($.sitemap({
+      siteUrl: baseUrl,
+      changefreq: "monthly"
+    }))
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task("html:writeNavigation", () => {
+  navigation = new Navigation(require(paths.assets + "pages/site-structure.json"));
+  fs.writeFileSync(paths.assets + "pages/siteOverviewList.pug", navigation.writeNavigation("allplain"));
+  fs.writeFileSync(paths.assets + "pages/topnavigation.pug", navigation.writeNavigation("top"));
+  fs.writeFileSync(paths.assets + "pages/footernavigation.pug", navigation.writeNavigation("footer"));
+});
+
+gulp.task("html:generatePages", ["html:writeNavigation"], () => {
+  const scope = {
+    register: require(paths.assets + "pages/data/register.json"),
+    berichte: require(paths.assets + "pages/data/berichte.json"),
+    vorstand: require(paths.assets + "pages/data/vorstand.json"),
+    termine: require(paths.assets + "pages/data/termine.json"),
+
+    jugendRegister: require(paths.assets + "pages/data/jugend-register.json"),
+    news: require(paths.assets + "pages/data/news.json"),
+    galleries: require(paths.assets + "gallery/galleries.json"),
+
+    siteTitle: "Musikverein Wollbach 1866 e.V.",
+    baseUrl: baseUrl,
+    numberOfMusicians: 0
+  };
+
+  let getNumberOfMusicians = (register: IPerson[]) => {
+    let distinctNames: {[key: string]: boolean} = {};
+    register.map((p) => {
+        return p.name + " " + p.familyName;
+      }).forEach((p) => {
+        distinctNames[p] = true;
+    });
+    var numberOfMusicians = Object.keys(distinctNames).length;
+    if (!numberOfMusicians || numberOfMusicians < 30 || numberOfMusicians > 70) {
+      $.util.log("Unexpected number of musicians calculated:", numberOfMusicians);
+      throw ("Unexpected number of musicians calculated: " + numberOfMusicians);
+    }
+
+    return numberOfMusicians;
+  };
+
+  scope.numberOfMusicians = getNumberOfMusicians(scope.register);
+
+  var getScope = (file: File) => {
+    var filename = path.basename(file.path, path.extname(file.path));
+    return {
+      marked: marked,
+      moment: moment,
+      require: require,
+
+      isAmp: false,
+      isRelease: isRelease,
+      scope: scope,
+
+      referencedFile: filename,
+      breadcrumb: filename === "index" ? null : navigation.getBreadcrumb(filename, true)
+    };
+  };
+
+  return gulp.src(paths.assets + "pages/partials/**/*.pug")
+              .pipe($.replace(/^(\s*#+) /gm, "$1# "))
+              .pipe($.rename((path: path.ParsedPath): void => { path.ext = ".html"; }))
+              .pipe($.grayMatter())
+              .pipe($.data(getScope))
+              .pipe($.data((file: File) => {
+                searchIndex.add(file, (<any> file).data);
+              }))
+              .pipe($.pug())
+              .pipe($.flatten())
+              .pipe(gulp.dest(paths.dest))
+/*
+              // https://github.com/pugjs/pug/issues/2367
+              .pipe($.if(file.data.hasAmp, $.data(function(file) {
+                file.data.isAmp = true;
+                return file.data
+              })))
+              .pipe($.if(file.data.hasAmp, $.pug()))
+              .pipe($.if(file.data.hasAmp, gulp.dest(paths.dest + "amp/")))
+*/
+  ;
+});
+
+gulp.task("html:minify", () => {
+  return gulp.src(paths.dest + "**/*.html")
+              .pipe($.htmlmin({
+                sortAttributes: true,
+                removeComments: true,
+                collapseWhitespace: true,
+                collapseInlineTagWhitespace: true,
+                removeAttributeQuotes: true,
+                conservativeCollapse: true,
+                minifyJS: true,
+                minifyCSS: true
+              }))
+              .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task("search:index", () => {
+  fs.writeFileSync(paths.dest + "index.json", JSON.stringify(searchIndex.getResult()));
+});
+
+gulp.task("default", $.sequence("html:generatePages", "search:index"));
+gulp.task("release", $.sequence("html:generatePages", "sitemap", "html:minify", "search:index"));
