27ef5892c724686ed887314e385d521a5fda3ab5
Tiliavir <tiliavir@gmail.com>
2018-11-01 00:20:26 +0100

    added: _prod/beautified

---
+++ _prod/gulpfile.js
@@ -0,0 +1,27 @@
+(function (require) {
+  "use strict";
+
+  const gulp = require("gulp"),
+        $ = require("gulp-load-plugins")();
+
+  const paths = {
+    src: "./",
+    beauty: "./beautified/"
+  };
+
+  gulp.task("html:validate", function () {
+    return gulp.src(paths.src + "*.html")
+               .pipe($.w3cjs());
+  });
+
+  gulp.task("beautify", function () {
+    return gulp.src([paths.src + "amp/*.html", paths.src + "*.html", paths.src + "gallery/*.json"])
+               .pipe($.jsbeautifier({
+                 indentSize: 2,
+                 mode: "VERIFY_AND_WRITE"
+               }))
+               .pipe(gulp.dest(paths.beauty));
+  });
+
+  gulp.task("default", ["html:validate", "beautify"]);
+})(require);
