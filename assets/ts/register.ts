c33460f13346eee7e39ff62318bcf0c7da23c2ab
tiliavir <tiliavir@users.noreply.github.com>
2023-01-30 21:54:27 +0100

    preventing infinite loop

--- assets/ts/register.ts
+++ assets/ts/register.ts
@@ -17,22 +17,36 @@ class Register {
     "Tuba": ["tuba_1.jpg", "tuba_2.jpg"]
   };
 
-  public static tryReplaceImage($images: JQuery): boolean {
-    const $image: JQuery = $($images[Math.floor(Math.random() * $images.length)]);
-    if (Register.isElementInViewport($image[0])) {
-      const remainingRegImageUrls = Register.register[$image.attr("title")]
-                                            .filter(url => !Register.endsWith("/" + $image.attr("src"), url));
-      if (remainingRegImageUrls.length > 0) {
-        $image.fadeOut("fast", (): void => {
-          const nexIndex = Math.floor(Math.random() * remainingRegImageUrls.length);
-          $image.attr("src", "/img/register/" + remainingRegImageUrls[nexIndex]);
-          $image.fadeIn("slow");
-        });
-        return true;
+  public static tryReplaceImage($images: JQuery): void {
+    let htmlElements = this.shuffle($images);
+    for (let image of htmlElements) {
+      if (Register.isElementInViewport(image)) {
+        const $image = $(image);
+        const remainingRegImageUrls = Register.register[$image.attr("title")]
+            .filter(url => !Register.endsWith("/" + $image.attr("src"), url));
+        if (remainingRegImageUrls.length > 0) {
+          $image.fadeOut("fast", (): void => {
+            const nexIndex = Math.floor(Math.random() * remainingRegImageUrls.length);
+            $image.attr("src", "/img/register/" + remainingRegImageUrls[nexIndex]);
+            $image.fadeIn("slow");
+          });
+          break;
+        }
       }
     }
-    return false;
   }
+
+  private static shuffle(array: JQuery): JQuery {
+    for (let m = array.length, t, i; m > 0; m--) {
+      i = Math.floor(Math.random() * m);
+
+      t = array[m - 1];
+      array[m - 1] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  };
 
   private static endsWith(str: string, suffix: string): boolean {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
@@ -49,11 +63,5 @@ class Register {
 
 $(() => {
   const $images = $(".mvw-register-table img");
-  setInterval(
-      (): void => {
-        while (!Register.tryReplaceImage($images)) {
-          // find an image to replace...
-        }
-      },
-      8000);
+  setInterval(() => Register.tryReplaceImage($images), 8000);
 });
